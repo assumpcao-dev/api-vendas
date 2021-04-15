535ec1bd65c15c86fe1af8430038ba81c1afb2c7
Rafael Assumpcao dos santos <assumpcao@Rafaels-MBP.lan>
2021-04-14 23:10:48 -0300

    Add Redis - RedisCache

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -3,12 +3,20 @@ import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 
+import RedisCache from '@shared/cache/RedisCache';
+
 export default class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
+
+    const redisCache = new RedisCache();
+
     const products = await productsRepository.find();
+
+    await redisCache.save('teste', 'teste');
+
     if (!products) {
-      throw new AppError('Cannot found product with this name...');
+      throw new AppError('Cannot found product with this name.');
     }
     return products;
   }
